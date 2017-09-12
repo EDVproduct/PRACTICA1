e39628579d9d7e8757c9414092b879dd9095e108
EDVproduct <davidvalles79@gmail.com>
2017-09-12 13:18:18 +0200

    el arxiu connectar

---
+++ new.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "curriculum";
+$port="3306";
+
+
+
+try
+{
+  $conn = new PDO("mysql:host=$host;port=$port;dbname=$db",$user,$pass);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connectad<p>";
+}
+catch (PDOException $e)
+{
+  echo "imposible de connectar: " . $e->getMessage() ."<p>";
+}
+ ?>
